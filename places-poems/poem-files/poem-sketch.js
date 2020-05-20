12d6f66b487a570e5cd1016bf48e1382e01250c8
Poire Molle <lapoiremolle@gmail.com>
2020-05-20 20:14:33 +0200

    adding an image array that should be called by the dropdownMonth select menu value

--- places-poems/poem-files/poem-sketch.js
+++ places-poems/poem-files/poem-sketch.js
@@ -10,12 +10,16 @@ var yearWordsNoun = ["a thunderous desire.", "a drop of water falling from a bra
 
 var dayPhrases = ["how can time be so liquid?", "it passed like a train going on to other places", "count to ten if you're feeling lonely", "I read the same book twice", "we saw movies, we ate breakfast", "it's the sound of the highway, not the atmosphere", "who's to say that we are not in love?", "your skin is soft and it holds no temples", "there was no way we'd make it, i walked you home", "it was ok in the end", "where have all the fire escapes been moved to?", "i didn't know about the trends", "your arm across my waist, a frame in my mind", "you unwrap my heart, slowly we tumble", "however you felt back then, however you feel now", "not long enough for it to break me", "on every road side, a lonely song", "somewhere for belonging", "i wish i knew", "reading books at ungodly hours", "lemon tea and cute porcelain", "nothing to write home about", "but it was enough", "i move along with the crowd", "but sometimes i look back and I see you, staring at the sun", "it was all over the newspapers", "it's sad but it also makes sense", "somehow there was a plan, i saw it on the map", "mischief and a jars of butter biscuits", "how many ways can a leaf fall to the ground", "behind the television set, obviously"];
 
-//let img;
-//function preload() {
-//    
-//    img = loadImage('jelly.jpg');
-//    
-//}
+var images = [];
+
+function preload() {
+    for (var i = 0; i < 4; i++) {
+        
+    images[i] = loadImage("back" + i + ".jpg");
+    
+}
+}
+    
 
     
 function setup() { 
@@ -63,13 +67,14 @@ function setup() {
 
      background(100,50); 
     
-    
+    preload();
 } 
 
 
 
 function displayPoem() {
-//  image(img, 0, 0);
+  image(image[parseInt(dropdownMonth.value()], 0, 0);
+              
    fill(0);
    textFont('monospace');
     
