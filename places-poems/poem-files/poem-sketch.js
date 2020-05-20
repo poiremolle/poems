d8793d7f2cc2c1c2bcf8b7b8c37ed1afc268fb09
Poire Molle <lapoiremolle@gmail.com>
2020-05-20 23:21:10 +0200

    making image preload for loop go to 12

--- places-poems/poem-files/poem-sketch.js
+++ places-poems/poem-files/poem-sketch.js
@@ -15,7 +15,7 @@ var dayPhrases = ["how can time be so liquid?", "it passed like a train going on
 var images = [];
 
 function preload() {
-    for (var i = 0; i < 4; i++) {
+    for (var i = 0; i < 12; i++) {
         
     images[i] = loadImage("back" + i + ".jpg");
     
